16861daa406528fa70c16de87b54f8d06adef4b7
alftacmir <alum.ataconm.2021@iesalixar.org>
2023-11-02 18:31:45 +0100

    MI PRIMER COMMIT

---
+++ babyonroad/src/main/java/com/ecoplus/babyonroad/BabyonroadApplication.java
@@ -0,0 +1,13 @@
+package com.ecoplus.babyonroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyonroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyonroadApplication.class, args);
+	}
+
+}
---
+++ babyonroad/src/test/java/com/ecoplus/babyonroad/BabyonroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoplus.babyonroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyonroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
